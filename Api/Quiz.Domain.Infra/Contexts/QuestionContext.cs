2d55fe0c64f1c47d729544f180db956f5c0b74d6
lfbardi <luisfbardi@gmail.com>
2020-06-13 13:21:24 -0300

    backend / Infra - Question data context

---
+++ Api/Quiz.Domain.Infra/Contexts/QuestionContext.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Quiz.Domain.Entities;
+
+namespace Quiz.Domain.Infra.Contexts
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Question> Questions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Question>().Property(question => question.Id);
+            modelBuilder.Entity<Question>().Property(question => question.Title).HasMaxLength(160).HasColumnType("varchar(160)");
+            modelBuilder.Entity<Question>().Property(question => question.CorrectAnswer).HasMaxLength(160).HasColumnType("varchar(80)");
+            modelBuilder.Entity<Question>().Property(question => question.AnswerA).HasMaxLength(160).HasColumnType("varchar(80)");
+            modelBuilder.Entity<Question>().Property(question => question.AnswerB).HasMaxLength(160).HasColumnType("varchar(80)");
+            modelBuilder.Entity<Question>().Property(question => question.AnswerC).HasMaxLength(160).HasColumnType("varchar(80)");
+            modelBuilder.Entity<Question>().Property(question => question.AnswerD).HasMaxLength(160).HasColumnType("varchar(80)");
+        }
+    }
+}
