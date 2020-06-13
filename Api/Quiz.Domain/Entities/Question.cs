7b8111a9112bab4999917fb87397d7cdda83265b
lfbardi <luisfbardi@gmail.com>
2020-06-13 13:04:50 -0300

    backend / Domain - entities

---
+++ Api/Quiz.Domain/Entities/Question.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Quiz.Domain.Entities
+{
+    public class Question : Entity
+    {
+        public Question(string title, string correctAnswer, string answerA,
+            string answerB, string answerC, string answerD)
+        {
+            Title = title;
+            CorrectAnswer = correctAnswer;
+            AnswerA = answerA;
+            AnswerB = answerB;
+            AnswerC = answerC;
+            AnswerD = answerD;
+        }
+        public string Title { get; private set; }
+        public string CorrectAnswer { get; private set; }
+        public string AnswerA { get; private set; }
+        public string AnswerB { get; private set; }
+        public string AnswerC { get; private set; }
+        public string AnswerD { get; private set; }
+        
+    }
+}
