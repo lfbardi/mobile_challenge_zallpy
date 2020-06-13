43fe134349c840cf1f307f1333e487e03d677e55
lfbardi <luisfbardi@gmail.com>
2020-06-13 13:21:58 -0300

    backend / Infra - migrations

---
+++ Api/Quiz.Domain.Infra/Migrations/20200613142028_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Quiz.Domain.Infra.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Title = table.Column<string>(type: "varchar(160)", maxLength: 160, nullable: true),
+                    CorrectAnswer = table.Column<string>(type: "varchar(80)", maxLength: 160, nullable: true),
+                    AnswerA = table.Column<string>(type: "varchar(80)", maxLength: 160, nullable: true),
+                    AnswerB = table.Column<string>(type: "varchar(80)", maxLength: 160, nullable: true),
+                    AnswerC = table.Column<string>(type: "varchar(80)", maxLength: 160, nullable: true),
+                    AnswerD = table.Column<string>(type: "varchar(80)", maxLength: 160, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Questions");
+        }
+    }
+}
